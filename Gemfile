e7849d714956ab0bdaf77ea38eee1d1e866476a7
Catarina Herculano <catarinaherculano@Catarinas-MacBook-Air.local>
2020-05-11 23:53:22 +0100

    Added icon finally

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem "jekyll", "~> 4.0.1"
 # gem "minima", "~> 2.5"
 gem "jekyll-theme-basically-basic"
 
-
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
 # gem "github-pages", group: :jekyll_plugins
---
+++ _site/--help/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "a post with formatting and links",
+        "excerpt":"Jean shorts raw denim Vice normcore, art party High Life PBR skateboard stumptown vinyl kitsch. Four loko meh 8-bit, tousled banh mi tilde forage Schlitz dreamcatcher twee 3 wolf moon. Chambray asymmetrical paleo salvia, sartorial umami four loko master cleanse drinking vinegar brunch. Pinterest DIY authentic Schlitz, hoodie Intelligentsia butcher...","categories": [],
+        "tags": [],
+        "url": "http://catarinacbh.com/2015/03/15/welcome-to-jekyll.html"
+      }]
